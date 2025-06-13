0539b4e638f40f92a030c1cc0920c68cef37a830
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 00:53:09 +0500

    Add king class declaration with constructor and main methods

---
+++ BSCS23185A-Chess-OOP/King.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include"Piece.h"
+#include<iostream>
+class king :public Piece
+{
+public:
+	char sym;
+	king(int _r, int _c, board* b, Color c, char ch);
+	virtual void drawpieces(int sr, int sc, int clr, int a);
+	virtual bool islegalmove(int sr, int sc, int dr, int dc, int turn, board* b, player* pla, int psr, int psc, int pdr, int pdc);
+	virtual char getpiecesymbol(int i, int j);
+	virtual bool getcaslaval();
+	virtual void setcaslaval(bool _set);
+};
+
