3a2357b1ee538cc1b5f79bb09d34e0d175d7312a
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 20:10:31 +0500

    Initializes turn, board, and both players with name, color, and time.

---
+++ BSCS23185A-Chess-OOP/Chess.cpp
@@ -0,0 +1,27 @@
+#include "chess.h"
+#include"Utility.h"
+#include"rook.h"
+#include"knight.h"
+#include"bishop.h"
+#include"queen.h"
+#include<stack>
+#include<chrono>
+#include<iomanip>
+#include<vector>
+
+
+using namespace std::chrono_literals;
+chess::chess(std::string p1, std::string p2, int ti)
+{
+	this->sr = 0;
+	this->sc = 0;
+	this->dr = 0;
+	this->dc = 0;
+
+	this->turn = WHITETURN;
+
+	this->parray[0] = new player(p1, BLACK, ti);
+	this->parray[1] = new player(p2, WHITE, ti);
+
+	this->b = new board();
+}
