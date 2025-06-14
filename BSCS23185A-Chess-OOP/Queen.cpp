14566d76662c6e0a9c23c9d78c7d9ba16e53dea0
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 18:17:16 +0500

    Implement drawpieces to visually display queen on board

--- BSCS23185A-Chess-OOP/Queen.cpp
+++ BSCS23185A-Chess-OOP/Queen.cpp
@@ -6,3 +6,34 @@ queen::queen(int _r, int _c, board* b, Color c, char ch)
     this->sym = ch;
     this->casaval = false;
 }
+
+void queen::drawpieces(int sr, int sc, int clr, int a)
+{
+    /*if (this->C == BLACK)
+    {
+        std::cout << "q";
+    }
+
+    if (this->C == WHITE)
+    {
+    std:: cout << "Q";
+    }*/
+    SetClr(clr);
+    for (int i = 0; i < a - 2; i++)
+    {
+        gotoRowCol(sr, sc - 1 + i);
+        std::cout << char(-37);
+    }
+
+    for (int j = 0; j < a - 2; j++)
+    {
+        gotoRowCol(sr - 2 + j, sc);
+        std::cout << char(-37);
+    }
+    for (int i = 0; i < a; i++)
+    {
+        gotoRowCol(sr + 1, sc - 2 + i);
+        std::cout << char(-37);
+    }
+
+}
