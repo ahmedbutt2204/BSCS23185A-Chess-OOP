b38a086e3fa2b5a442e608b78838a0a97ebd4869
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 19:56:09 +0500

    Implement displaytime() to show timer for each player with formatting

--- BSCS23185A-Chess-OOP/Player.cpp
+++ BSCS23185A-Chess-OOP/Player.cpp
@@ -19,3 +19,19 @@ Color player::getcolor()
 {
 	return this->C;
 }
+
+void player::displaytime(int _t)
+{
+	if (_t == BLACK)
+	{
+		gotoRowCol(19, 70);
+		std::cout << std::setfill('0') << std::setw(2) << int(this->time * 0.0167) % 4 << " : " << std::setfill('0') << std::setw(2) << this->time % 60;
+	}
+
+	if (_t == WHITETURN)
+	{
+		gotoRowCol(64, 70);
+		std::cout << std::setfill('0') << std::setw(2) << int(this->time * 0.0167) % 4 << " : " << std::setfill('0') << std::setw(2) << this->time % 60;
+	}
+}
+
