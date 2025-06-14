a7a37c70ea224c3136dfec73ba109fd7cab07de7
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 14:56:55 +0500

    created header file for pawn piece

---
+++ BSCS23185A-Chess-OOP/Pawn.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"Piece.h"
+class pawn :public Piece
+{
+public:
+	char sym;
+	pawn(int _r, int _c, board* b, Color c, char ch);
+	virtual void drawpieces(int sr, int sc, int clr, int a);
+	virtual bool islegalmove(int sr, int sc, int dr, int dc, int turn, board* b, player* pla, int psr, int psc, int pdr, int pdc);
+	virtual char getpiecesymbol(int i, int j);
+	virtual bool getcaslaval();
+	virtual void setcaslaval(bool _set);
+};
+
+  
