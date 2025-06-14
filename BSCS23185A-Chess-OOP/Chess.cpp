040b14cb4d56783c30a34f79b9c6dfc4db28d7c9
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 20:11:59 +0500

    Add functions to draw empty visual boxes for move highlighting

--- BSCS23185A-Chess-OOP/Chess.cpp
+++ BSCS23185A-Chess-OOP/Chess.cpp
@@ -31,3 +31,41 @@ void chess::displayturnmsg(player* p)
 	gotoRowCol(26, 75);
 	std::cout << p->getpnamecolr() << " turns " << std::endl;
 }
+
+void emptybox(int Sr, int Sc, int height, int width, int clr)
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if ((i == 0 || i == height - 1) && (j == 0 || j == 1 || j == 4 || j == 5))
+			{
+				gotoRowCol(i + Sr, j + Sc);
+				SetClr(clr);
+				std::cout << char(-37);
+			}
+
+			if ((i == 1 || i == 4 || i == 3) && (j == 0 || j == width - 1))
+			{
+				gotoRowCol(i + Sr, j + Sc);
+				SetClr(clr);
+				std::cout << char(-37);
+			}
+
+			// previous one :
+			/*if (i == 0 || j == 0 || i == height - 1 || j == width - 1)
+			{
+
+				gotoRowCol(i + Sr, j + Sc);
+				SetClr(clr);
+				std::cout << char(-37);
+			}*/
+		}
+	}
+
+	SetClr(WHITE);
+}
+
+
+
+
