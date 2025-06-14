da4ecfcbc2da7df82ed5cf65a0d60dcb5ed8a161
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 18:25:33 +0500

    Draw rook piece using box pattern in console

--- BSCS23185A-Chess-OOP/Rook.cpp
+++ BSCS23185A-Chess-OOP/Rook.cpp
@@ -8,3 +8,24 @@ rook::rook(int _r, int _c, board* b, Color c, char ch)
     this->sym = ch;
     this->casaval = true;
 }
+
+void rook::drawpieces(int sr, int sc, int clr, int a)
+{
+    /*if (this->C == BLACK)
+    {
+        std::cout << "r";
+    }
+
+    if (this->C == WHITE)
+    {
+        std::cout << "R";
+    }*/
+    SetClr(clr);
+    for (int i = 0; i < a - 2; i++)
+    {
+        for (int j = 0; j < a - 2; j++)
+        {
+            gotoRowCol(sr - 1 + i, sc - 1 + j);
+            std::cout << char(-37);
+        }
+    }
