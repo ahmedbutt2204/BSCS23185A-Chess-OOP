4f581972f07a17eb444db4684284187622bbc9b6
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 18:32:13 +0500

    Declare board constructor, copy constructor, and assignment operator

--- BSCS23185A-Chess-OOP/Board.h
+++ BSCS23185A-Chess-OOP/Board.h
@@ -6,3 +6,10 @@ class board
 {
 private:
 	Piece*** ps;
+
+public:
+	board();
+	board(const board& h1);
+	const board* operator=(const board* h1);
+	void drawbox(int sr, int sc, int height, int width, int clr, int p1, int p2);
+	void displayboard(int height, int width);
