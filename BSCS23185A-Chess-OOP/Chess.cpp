7ed7498de375e555d50bb3081060f32402d728e3
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 20:12:35 +0500

    emptybox/unemptybox: highlights and un-highlights board squares.

--- BSCS23185A-Chess-OOP/Chess.cpp
+++ BSCS23185A-Chess-OOP/Chess.cpp
@@ -66,6 +66,48 @@ void emptybox(int Sr, int Sc, int height, int width, int clr)
 	SetClr(WHITE);
 }
 
+void unemptybox(int Sr, int Sc, int height, int width, int clr)
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			/*if ((i == 0 || i == height - 1) && (j == 0 || j == 1 || j == 4 || j == 5))
+			{
+				if ((i + j % 2) == 0)
+				{
+					SetClr(YELLOW);
+				}
+				gotoRowCol(i + Sr, j + Sc);
+
+				std::cout << char(-37);
+			}
+
+			if ((i == 1 || i == 4 || i == 3) && (j == 0 || j == width - 1))
+			{
+				gotoRowCol(i + Sr, j + Sc);
+
+				if ((i + j % 2) == 1)
+				{
+					SetClr(GREEN);
+				}
+				std::cout << char(-37);
+			}*/
+
+			// previous one :
+			if (i == 0 || j == 0 || i == height - 1 || j == width - 1)
+			{
+
+				gotoRowCol(i + Sr, j + Sc);
+				SetClr(clr);
+				std::cout << char(-37);
+			}
+		}
+	}
+
+	SetClr(WHITE);
+}
+
 
 
 
