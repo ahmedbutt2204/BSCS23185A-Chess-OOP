2543047b390612a9bef939b064dd1b4f46de4123
Ahmed Nadeem <ahmedbutt2204@gmail.com>
2025-06-14 18:33:24 +0500

    Declare methods to draw board cells and display full board

--- BSCS23185A-Chess-OOP/Board.h
+++ BSCS23185A-Chess-OOP/Board.h
@@ -13,3 +13,5 @@ public:
 	const board* operator=(const board* h1);
 	void drawbox(int sr, int sc, int height, int width, int clr, int p1, int p2);
 	void displayboard(int height, int width);
+	Piece* getpiece(int i, int j);
+	Piece*& getpieceem(int i, int j);
